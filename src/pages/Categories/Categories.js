62d8e788d65850a8becffdc53e460bc49ea9335a
Kavalorn <kavalorn@ex.ua>
2019-02-14 15:04:33 +0300

    pages and report components refactoring

---
+++ src/pages/Categories/Categories.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import ApiCategories from '../../services/Categories';
+import Modal from '../../components/Modal';
+
+class Categories extends Component {
+    _apiCategories = new ApiCategories();
+    state = {
+        categories: null,
+        modalOpen: false
+    };
+
+    componentDidMount() {
+        this.getData();
+    };
+
+    postCategory = (category) => {
+        this._apiCategories
+            .addCategory(category)
+            .then(res => this.setState(prevState => ({ categories: [...prevState.categories, res] })));
+
+        this.toggleModal();
+    };
+
+    getData = () => {
+        this._apiCategories
+            .getCategories()
+            .then(res => this.setState({ categories: res }));
+    };
+
+    toggleModal = () => this.setState({ modalOpen: !this.state.modalOpen });
+
+    render() {
+        const { categories, modalOpen } = this.state;
+        const { toggleModal, postCategory } = this.state;
+
+        return (
+            <div>
+                <button className="btn btn-primary" onClick={toggleModal}>Add Category</button>
+                <table className="table table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">Id</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {categories ? categories.map(({ id, name, active }) => (
+                            <tr className="table-active" key={id}>
+                                <th scope="row">{id}</th>
+                                <td>{name}</td>
+                                <td>{active ? 'active' : 'inactive'}</td>
+                            </tr>
+                        )) : null}
+                    </tbody>
+                </table>
+                {modalOpen ? <Modal toggleModal={toggleModal} postCategory={postCategory} /> : null}
+            </div>
+        )
+    }
+}
+
+export default Categories;
---
+++ src/pages/Items/Items.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class Items extends Component {
+
+    render() {
+        return (
+            <div>
+                <table className="table table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">Type</th>
+                            <th scope="col">Column heading</th>
+                            <th scope="col">Column heading</th>
+                            <th scope="col">Column heading</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className="table-active">
+                            <th scope="row">Active</th>
+                            <td>Column content</td>
+                            <td>Column content</td>
+                            <td>Column content</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
+
+export default Items;
